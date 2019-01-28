f09e5b3b392efdc50d8cbb6563ca59dde8cd546b
Naresh <naresh.adepu@vertiv.com>
2019-01-29 01:14:27 +0530

    Dynamic Form Added

--- app/src/views/Form.vue
+++ app/src/views/Form.vue
@@ -31,18 +31,34 @@
         </div>
       </div>
 
-
       <div class="col-6">
+        <select v-model="category" v-on:change="onChange" id="select1" class="form-control">
+          <option value="fruits">Fruits</option>
+          <option value="vegetables">Vegetables</option>
+        </select>
+        
+        <select id="select2" v-model="list" class="form-control">
+          <option
+            v-for="option in options"
+            v-bind:value="option.value"
+            :key="option"
+          >{{ option.text }}</option>
+        </select>
 
-<select v-model = "category" v-on:change="onChange" id="select1" class="form-control" >
-    <option value="fruits">Fruits</option>
-    <option value="vegetables">Vegetables</option>
-                            
-</select>
-
-<select id="select2" v-model= "list" class="form-control">
-    <option  v-for="option in options" v-bind:value="option.value" :key="option"> {{ option.text }} </option>
-</select>
+        <div class="card mt-3" v-for="(item1 ,index) in itemss" :key="item1.id">
+          <div class="card-body">
+            <h5 class="card-title">
+              <span style="cursor:pointer" class="float-right" @click="delete1(index)">x</span>
+            </h5>
+            <div class="form-group">
+              <input type="text" v-model="item1.test1" class="form-control" placeholder="test1">
+            </div>
+            <div class="form-group">
+              <input type="text" v-model="item1.test2" class="form-control" placeholder="test2">
+            </div>
+          </div>
+        </div>
+        <button class="btn btn-secondary btn-sm mt-3" @click="addItem()">Add new item</button>
       </div>
     </div>
   </div>
@@ -52,6 +68,7 @@ export default {
   name: "Form",
   data: function() {
     return {
+      itemss: [],
       employess: [],
       data: {
         employess: [
@@ -60,51 +77,53 @@ export default {
             email: "",
             something: ""
           }
-        ]
+        ],
+        itemss: [{ message: "Foo" }, { message: "Bar" }]
       },
-       category: 'fruits', 
-        
-        list: '',
+      category: "fruits",
 
-           optionsData: {
-               fruits: [
-                    { text: 'Orange', value: 'orange' },
-                    { text: 'Banane', value: 'banana' },
-               ],
-               
-               vegetables: [
-                   { text: 'Brocolis', value: 'brocolis' },
-                   { text: 'Radish', value: 'radish' },
-               ]
-           }
+      list: "",
+
+      optionsData: {
+        fruits: [
+          { text: "Orange", value: "orange" },
+          { text: "Banane", value: "banana" }
+        ],
+
+        vegetables: [
+          { text: "Brocolis", value: "brocolis" },
+          { text: "Radish", value: "radish" }
+        ]
+      }
     };
   },
-   computed: {
-       
-           options: function() {
-           
-           let options = ''
+  computed: {
+    options: function() {
+      let options = "";
 
-                switch(this.category) {
+      switch (this.category) {
+        case "fruits":
+          options = this.optionsData.fruits;
+          break;
 
-                    case 'fruits':
-                    options = this.optionsData.fruits
-                    break;
-                    
-                    case 'vegetables':
-                    options = this.optionsData.vegetables
-                    break;
+        case "vegetables":
+          options = this.optionsData.vegetables;
+          break;
 
-                    default:
-                    options = this.optionsData.fruits
-                }
+        default:
+          options = this.optionsData.fruits;
+      }
 
-                return options
-           
-           }
-       
-       },
+      return options;
+    }
+  },
   methods: {
+    addItem() {
+      this.itemss.push({
+        test1: "",
+        test2: ""
+      });
+    },
     addNewEmployeeForm() {
       this.employess.push({
         name: "",
@@ -115,10 +134,14 @@ export default {
     deleteEmployeeForm(index) {
       this.employess.splice(index, 1);
     },
-    onChange: function ()
-         {
-             this.options = this.options
-         }
+
+    delete1(index) {
+      this.itemss.splice(index, 1);
+    },
+
+    onChange: function() {
+      this.options = this.options;
+    }
   }
 };
 </script>
